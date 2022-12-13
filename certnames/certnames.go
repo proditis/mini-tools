ef87a9d391057ca701ae952ba050cec5db9e0a9e
Pantelis Roditis <proditis@echothrust.com>
2022-12-13 13:24:42 +0200

    add a few more options this will ease processing of bulk files as well as easy grep and parse of the output

--- certnames/certnames.go
+++ certnames/certnames.go
@@ -3,37 +3,109 @@ package main
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatalln("Missing pem filename")
-	}
-	certFile := os.Args[1]
-	bs, err := os.ReadFile(certFile) // handle error
+var version = "X.X.X" // populated by build script
+// holds the args supplied to the program
+type commandArgs struct {
+	Debug          bool
+	IncludeSubject bool
+	DoDNS          bool
+}
+
+// Display usage then exit
+func usage() {
+	usage := `Usage: certnames [Options] files`
+	usage += `
+Extracts Subject Alt Names from x509 PEM format certificate(s) from a list of files
+  certnames v` + version + `
+Options:
+  -h, --help    show this help message and exit
+  -s, --subject include subject display
+  -d, --dns     do dns on subject names
+`
+	fmt.Fprint(flag.CommandLine.Output(), usage)
+	os.Exit(0)
+}
+
+// Helper func to handle errors that should exit the program
+func handlErrFatal(err error) {
 	if err != nil {
-		log.Fatalf("File %s not found", certFile)
+		log.Fatal("[!] ", err)
 	}
-	block, _ := pem.Decode(bs)
-	if block == nil {
-		log.Fatal("failed to parse PEM block containing the public key")
+}
+
+// Check that the args passed are valid, returns either the host string or shows usage
+func checkArgs() {
+	if len(os.Args) < 2 {
+		fmt.Fprint(flag.CommandLine.Output(), "Missing pem filename\n")
+		usage()
 	}
+}
 
-	cert, err := x509.ParseCertificate(block.Bytes) // handle error
-
-	fmt.Printf("Subject:   %v\n", cert.Subject)
-	fmt.Printf("DNS names: %+v\n", cert.DNSNames)
-
-	for i := 0; i < len(cert.DNSNames); i++ {
-		ips, err := net.LookupIP(cert.DNSNames[i])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s no dns\n", cert.DNSNames[i])
-		} else {
-			fmt.Printf("%s %-v\n", cert.DNSNames[i], ips)
+func lookupHelper(DNSNames string) []string {
+	var ipStr []string
+	ips, err := net.LookupIP(DNSNames)
+	if err == nil {
+		for _, lip := range ips {
+			ipStr = append(ipStr, lip.String())
 		}
 	}
+	return ipStr
+}
+func main() {
+	args := commandArgs{}
+	log.SetFlags(0)
+	log.SetPrefix("")
+
+	// setup and process args
+	flag.BoolVar(&args.Debug, "debug", false, "debug output")
+	flag.BoolVar(&args.IncludeSubject, "s", false, "include cetificate subject")
+	flag.BoolVar(&args.IncludeSubject, "subject", false, "include cetificate subject")
+	flag.BoolVar(&args.DoDNS, "d", false, "do DNS on names")
+	flag.BoolVar(&args.DoDNS, "dns", false, "do DNS on names")
+	flag.Usage = usage
+	flag.Parse()
+	checkArgs()
+
+	for _, certFile := range flag.Args() {
+		if args.Debug {
+			fmt.Println(certFile)
+		}
+
+		bs, err := os.ReadFile(certFile) // handle error
+		if err != nil {
+			fmt.Fprintf(flag.CommandLine.Output(), "File %s not found", certFile)
+			continue
+		}
+
+		block, _ := pem.Decode(bs)
+		if block == nil {
+			log.Println("failed to parse PEM block containing the public key")
+			continue
+		}
+		cert, err := x509.ParseCertificate(block.Bytes) // handle error
+		if args.IncludeSubject {
+			fmt.Printf("# Subject:   %v\n", cert.Subject)
+		}
+		//fmt.Printf("DNS names: %+v\n", cert.DNSNames)
+		for i := 0; i < len(cert.DNSNames); i++ {
+			fmt.Fprintf(flag.CommandLine.Output(), "%s", cert.DNSNames[i])
+			if args.DoDNS {
+				ips := lookupHelper(cert.DNSNames[i])
+				if len(ips) > 0 {
+					fmt.Fprintf(flag.CommandLine.Output(), " %s", strings.Join(ips, " "))
+				}
+			}
+			fmt.Fprintln(flag.CommandLine.Output(), "")
+		}
+		fmt.Fprintln(flag.CommandLine.Output(), "")
+	}
+
 }
